1776c2c25f9076164a65e35bcf59b38a264f5ce8
trong <devgruxcon@gmail.com>
2015-09-06 07:33:56 -0400

    predict price using normal equation

--- machine-learning-ex1/ex1/predictPrice.m
+++ machine-learning-ex1/ex1/predictPrice.m
@@ -4,6 +4,7 @@ function p = predictPrice(size, beds, theta, mu, sigma)
 n_size = (size - mu(1)) / sigma(1)
 n_beds = (beds - mu(2)) / sigma(2)
 
+% Add 1 for constant feature
 p = [1 n_size n_beds] * theta
 
 end
