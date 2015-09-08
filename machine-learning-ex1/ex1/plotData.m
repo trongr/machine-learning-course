ee10f6bbd3168ce2acce0096b59b34503223d157
trong <devgruxcon@gmail.com>
2015-09-08 16:11:26 -0400

    proper figure hold on hold off order

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -14,15 +14,17 @@ function plotData(x, y)
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
+figure; % open a new figure window
+hold on;
+
 plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
 ylabel('Profit in $10,000s'); % Set the y􀀀axis label
 xlabel('Population of City in 10,000s'); % Set the x􀀀axis label
 
-figure; % open a new figure window
 
 
 
-
+hold off;
 
 
 % ============================================================
