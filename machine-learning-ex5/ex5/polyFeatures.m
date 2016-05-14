4542ee8543c19be7d9c982833deed7ac2c761759
trong <devgruxcon@gmail.com>
2016-05-14 04:40:18 -0400

    ex5: polynomial features

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -7,17 +7,21 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
+% X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+% Instructions: Given a vector X, return a matrix X_poly where the p-th
 %               column of X contains the values of X to the p-th power.
 %
-% 
+%
 
+A = repmat(X, 1, p); % duplicate X into matrix of p columns
+b = linspace(1, p, p); % b = [1, 2, 3,..., p];
 
+% take each column of A and raise it to the corresponding elements of b. see
+% http://stackoverflow.com/questions/2307249/how-to-apply-a-function-to-every-row-in-matlab
 
-
+X_poly = bsxfun(@(x,y) x.^y, A, b);
 
 
 % =========================================================================
