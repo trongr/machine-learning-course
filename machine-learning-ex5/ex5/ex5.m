8054d52baac9768ad81e343595fbb31b83f5c1b4
trong <devgruxcon@gmail.com>
2016-05-15 23:18:37 -0400

    ex5: learning curve on randomized training and validation sets

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -222,7 +222,26 @@ best_lambda = 3;
 J_test = testBestLambda(X_poly, y, X_poly_test, ytest, best_lambda);
 fprintf("test error %f using best_lambda %f\n", J_test, best_lambda);
 
-
-
 fprintf('Program paused. Press enter to continue.\n');
+
+pause;
+
+lambda = 0.01;
+[error_train, error_val] = ...
+    learningCurveRandomizedSamples([ones(m, 1) X], y, ...
+                                   [ones(size(Xval, 1), 1) Xval], yval, ...
+                                   lambda);
+
+plot(1:m, error_train, 1:m, error_val);
+title('Learning curve with randomized samples for linear regression')
+legend('Train', 'Cross Validation')
+xlabel('Number of training examples')
+ylabel('Error')
+axis([0 13 0 150])
+
+fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+for i = 1:m
+    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+end
+
 pause;
